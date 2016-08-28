bf0a9a528808bdc0868149c10652f3a165f90385
mrjackinc <mrjackinc@gmail.com>
2016-08-27 22:11:14 -0700

    Can display Polyminis in txt, changed morphology to use HashMaps

--- src/morphology.rs
+++ src/morphology.rs
@@ -1,11 +1,12 @@
-use std::collections::{HashSet};
+use std::collections::{HashSet, HashMap};
 use std::cmp::{min, max};
+use std::fmt;
 
 //
 //
 pub type Coord = (i32, i32);
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug)]
 pub enum Directions
 {
     UP,
@@ -31,8 +32,6 @@ impl Gene
         let ap = (((0xFF << 16) & byte) >> 16) as u8;
         let gp = (0xFFFF & byte) as u16;
 
-        println!("CP: {} AP: {} GP: {}", cp, ap, gp);
-
         Gene { control_payload: cp,
                adjacency_payload: ap,
                genetic_payload: gp,
@@ -43,6 +42,7 @@ impl Gene
 
 //
 //
+#[derive(Debug)]
 pub struct AdjacencyInfo
 {
     adj: Vec<Directions>
@@ -89,6 +89,13 @@ impl Cell
         Cell { adjacency_info: adjacency_info }
     }
 }
+impl fmt::Debug for Cell
+{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result
+    {
+        write!(f, "X")
+    }
+}
 
 
 //
@@ -96,42 +103,149 @@ impl Cell
 struct Representation
 {
     cells: Vec<Cell>,
-    positions: [Vec<Coord>; 4]
+    positions: [HashMap<Coord, usize>; 4],
+    dimensions: (u8, u8),
+    corner: (i32, i32),
 }
 impl Representation
 {
-    pub fn new(positions: Vec<Coord>, cells: Vec<Cell>) -> Representation
+    pub fn new(positions: Vec<Coord>, cells: Vec<Cell>,
+               dimensions: (u8, u8), corner: (i32, i32)) -> Representation
     {
-        let mut all_positions : [Vec<Coord>; 4] = [vec![], vec![], vec![], vec![]];
+        let mut all_positions : [HashMap<Coord, usize>; 4] = [HashMap::new(), HashMap::new(),
+                                                              HashMap::new(), HashMap::new()];
 
-        all_positions[0] = positions;
+        for i in 0..positions.len()
+        {
+            all_positions[0].insert(positions[i], i);
+        }
+
         all_positions[1] = Representation::rotate(&all_positions[0]); 
         all_positions[2] = Representation::rotate(&all_positions[1]); 
         all_positions[3] = Representation::rotate(&all_positions[2]); 
 
-        Representation { cells: cells, positions: all_positions }
+        Representation { cells: cells, positions: all_positions, dimensions: dimensions,
+                         corner: corner }
     }
 
-    fn rotate(positions: &Vec<Coord>) -> Vec<Coord>
+    fn rotate(positions: &HashMap<Coord, usize>) -> HashMap<Coord, usize>
     {
-        let mut to_ret = vec![];
-        for c in positions 
+        let mut to_ret: HashMap<Coord, usize> = HashMap::new();
+        for (c, p) in positions.iter()
         {
             // Rotation matrix [ cosA  -sinA ]
             //                 [ sinA   cosA  ]
             // A = 90 degrees
-            to_ret.push( (c.1, -1 * c.0) );
+            let new_c = (c.1, -1 * c.0);
+            to_ret.insert(new_c, *p);
         }
         to_ret
+    }
+}
+impl fmt::Debug for Representation 
+{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result
+    {
+        let mut result = write!(f, "\n");
+        for i in 0..self.dimensions.1
+        {
+            for j in 0..self.dimensions.0
+            {
+                match self.positions[0].get(&(j as i32, i as i32))
+                {
+                    Some(x) => {
+                        result = write!(f, "{:?}", self.cells[*x]);
+                    },
+                    None => {
+                        result = write!(f, ".");
+                    }
+                }
+            }
+            result = write!(f, "\n");
+        }
+        result
     }
 }
 
 
 //
 //
+const TIER_ONE_CHAIN: u16 = 0xFF;
+const TIER_TWO_CHAIN: u16  = 0x0F;
+const TIER_THREE_CHAIN: u16 = 0x0F;
+struct TieredGene
+{
+    tier: u8,
+    trait_number: u16
+}
+impl TieredGene
+{
+    fn new(gene_payload: u16) -> TieredGene
+    {
+        //TODO: Make this configurable
+        //TIER I
+        let mut tier: u8 = 1;
+        let mut trait_num = gene_payload & (0xFF<<8) as u16;
+        if trait_num == TIER_ONE_CHAIN
+        {
+            tier += 1;
+            //TIER II
+            trait_num = (gene_payload & (0xF<<4)) as u16;
+            if trait_num == TIER_TWO_CHAIN
+            {
+                //TIER III
+                trait_num = (gene_payload & 0xF) as u16;
+            }
+        }
+
+        TieredGene
+        {
+            tier: tier,
+            trait_number: trait_num
+        }
+    }
+}
+
+
+//
+//
+trait PolyminiCellFactory
+{
+    fn create_for_gene(g: Gene) -> Cell;
+}
+
+struct BasicPolyminiCellFactory;
+impl PolyminiCellFactory for BasicPolyminiCellFactory
+{
+    fn create_for_gene(g: Gene) -> Cell
+    {
+        let v = g.adjacency_payload;
+        let dirs = vec![Directions::UP, Directions::DOWN, Directions::LEFT, Directions::RIGHT];
+        let mut adj_dirs = vec![];
+
+        for i in 0..4
+        {
+            if 1<<i & v != 0 
+            {
+                adj_dirs.push(dirs[i]);
+            }
+        }
+
+        let ai = AdjacencyInfo { adj: adj_dirs };
+         
+        let tg = TieredGene::new(g.genetic_payload); 
+
+        Cell::new(ai)
+    }
+}
+
+
+//
+//
+#[derive(Debug)]
 pub struct Morphology
 {
-    dimensions: Coord,
+    dimensions: (u8, u8),
     representations: Representation,
 }
 impl Morphology
@@ -139,30 +253,16 @@ impl Morphology
     pub fn new() -> Morphology
     {
         Morphology { dimensions: (0,0),
-                     representations: Representation { cells: vec![], positions: [vec![], vec![], vec![], vec![]] } }
+                     representations: Representation::new(vec![], vec![], (0,0), (0,0)) }
     }
 
     //TODO: This names wtf XD
-    fn construct(genes: Vec<Gene>, _: Option<i32>) -> Vec<Cell>
+    fn construct(genes: Vec<Gene>, _:Option<i8>) -> Vec<Cell>
     {
         let mut to_ret = vec![];
         for g in genes
         {
-            let v = g.adjacency_payload;
-            let dirs = vec![Directions::UP, Directions::DOWN, Directions::LEFT, Directions::RIGHT];
-            let mut adj_dirs = vec![];
-
-            for i in 0..4
-            {
-                if 1<<i & v != 0 
-                {
-                    adj_dirs.push(dirs[i]);
-                }
-            }
-
-            let ai = AdjacencyInfo { adj: adj_dirs };
-
-            to_ret.push(Cell::new(ai));
+            to_ret.push(BasicPolyminiCellFactory::create_for_gene(g));
         }
         to_ret
     }
@@ -213,22 +313,14 @@ impl Morphology
         let drain_cell: Vec<Cell> = cells.drain(0..l).collect();
         let drain_pos: Vec<Coord> = positions.drain(0..l).collect();
 
-        let w  = maxx - minx + 1;
-        let h  = maxy - miny + 1;
+        let w = (maxx - minx + 1) as u8;
+        let h = (maxy - miny + 1) as u8;
 
-        // Re-Iterate through the cells and placing them in the [0] representation
-        let r1 = Representation::new(drain_pos, drain_cell);
+        let r1 = Representation::new(drain_pos, drain_cell,
+                                     (w, h), (minx, miny));
         
         Morphology { dimensions: (w, h), representations: r1 }
     }
-}
-
-
-//
-//
-trait PolyminiCellFactory
-{
-    fn create_for_gene(&self, g: Gene) -> Cell;
 }
 
 
@@ -305,9 +397,9 @@ mod test
     {
         let v1: u32 = 0x09;
         let v2: u32 = 0x0B;
-        let genes = vec![Gene::new(v1 << 16),
-                         Gene::new(v2 << 16),
-                         Gene::new(0), Gene::new(0)];
+        let genes = vec![Gene::new((v1 << 16) + 0x6AAD),
+                         Gene::new((v2 << 16) + 0xBEDA),
+                         Gene::new(0xDEADBEEF), Gene::new(0x0600DBAD)];
         let cells = Morphology::construct(genes, None);
 
         let morph = Morphology::build(cells);
@@ -319,5 +411,7 @@ mod test
                 println!("{:?}", c)
             }
         }
+
+        println!("{:?}", morph);
     }
 }
